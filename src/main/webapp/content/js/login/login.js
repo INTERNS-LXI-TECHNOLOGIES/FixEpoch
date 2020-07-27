0bf4de72a5a2cd2cdf3636c727e11d6ae184b2c1
adarshlxi <adarsh.m.a@lxisoft.com>
2020-07-27 19:06:54 +0530

    http://jira.bytatech.com:8080/browse/IN-8705

--- src/main/webapp/content/js/login/login.js
+++ src/main/webapp/content/js/login/login.js
@@ -18,3 +18,14 @@ btn.onclick = function() {
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+var disableBodyScroll = function(){
+    window.body_scroll_pos = $(window).scrollTop(); // write page scroll position in a global variable
+    $('body').css('overflow-y','hidden');
+}
+
+// Run this function when you close your popup:
+var enableBodyScroll = function(){
+    $('body').css('overflow-y','scroll');
+    $(window).scrollTop(window.body_scroll_pos); // restore page scroll position from the global variable
+}
