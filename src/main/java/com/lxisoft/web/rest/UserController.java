3fbc52b73395ecc8ab454c68df7ef48c6921392f
rahulrjr <rahul.m.p@lxisoft.com>
2020-07-28 10:06:06 +0530

    neww

--- src/main/java/com/lxisoft/web/rest/UserController.java
+++ src/main/java/com/lxisoft/web/rest/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-     @GetMapping(value = "/home")
+    @GetMapping(value = "/home")
     public String home(){
         return "home";
     }
