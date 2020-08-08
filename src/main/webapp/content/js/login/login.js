45cbbfc8971786a1b1028c281003555637b5d112
adarshlxi <adarsh.m.a@lxisoft.com>
2020-08-08 18:46:44 +0530

    http://jira.bytatech.com:8080/browse/IN-9290

--- src/main/webapp/content/js/login/login.js
+++ src/main/webapp/content/js/login/login.js
@@ -4,15 +4,16 @@ function openForm() {
         disableBodyScroll();
         }
 function closeForm() {
-  document.getElementById("popupForm").style.display = "none";
-enableBodyScroll();
+        document.getElementById("popupForm").style.display = "none";
+        document.getElementById("transparent-div").style.display = "none";
+        enableBodyScroll();
 }
 
-var modal = document.getElementById("popupForm");
+let modal = document.getElementById("popupForm");
 
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("myBtn");
 
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 
 
@@ -20,13 +21,14 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-var disableBodyScroll = function(){
+let disableBodyScroll = function(){
     window.body_scroll_pos = $(window).scrollTop(); // write page scroll position in a global variable
     $('body').css('overflow-y','hidden');
 }
 
 // Run this function when you close your popup:
-var enableBodyScroll = function(){
+let enableBodyScroll = function(){
     $('body').css('overflow-y','scroll');
     $(window).scrollTop(window.body_scroll_pos); // restore page scroll position from the global variable
 }
+
